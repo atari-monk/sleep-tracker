df9d351f4d59bd5e5960305ca7c1e1f9adc375c6
atari-monk <atari.monk1@gmail.com>
2025-04-15 14:38:43 +0200

    feat: add SleepRecord model and initial migration

--- sleep/models.py
+++ sleep/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SleepRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sleep_time = models.DateTimeField()
+    wake_time = models.DateTimeField()
+    notes = models.TextField(blank=True, null=True)
+
+    @property
+    def duration(self):
+        return self.wake_time - self.sleep_time
